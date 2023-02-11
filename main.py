f3bdf22833f56a8c9758441ef49a96bdda9ebf13
Er. Shubham Khatwase <78332999+shubhkhatwase@users.noreply.github.com>
2023-02-11 11:40:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from streamlit_option_menu import option_menu
 with st.sidebar:
      st.selectbox('Select operation:', ["Operators","List","Tuples","Dictionary"])
 st.title("Basic Python Operators: ")
